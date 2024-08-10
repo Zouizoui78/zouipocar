58fd1308b0d8820334e3a07e7535c76c0e8993e9
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-08-10 14:09:45 +0200

    fix speed integer size

--- server/src/Fix.hpp
+++ server/src/Fix.hpp
@@ -12,9 +12,9 @@ public:
     Fix(const std::vector<uint8_t>& packet);
 
     uint32_t timestamp = 0;
-    int64_t speed = 0;
     float latitude = 0;
     float longitude = 0;
+    uint8_t speed = 0;
 };
 
 }
