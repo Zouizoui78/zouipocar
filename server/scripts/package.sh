f96c6adef97fcfadfa1f42904bbaa237046c7295
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-08-23 09:33:38 +0200

    build release in package script

--- server/scripts/package.sh
+++ server/scripts/package.sh
@@ -1,3 +1,6 @@
+rm -r build
+cmake -B build -DCMAKE_BUILD_TYPE=release -DZOUIPOCAR_TESTS=OFF
+cmake --build build -j
 rm zouipocar.zip
 zip -r zouipocar.zip www
 zip -j zouipocar.zip build/zouipocar zouipocar.service
