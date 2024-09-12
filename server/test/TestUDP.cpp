d71cb32a5762d5fd07dbb2ed9afc7e806882d803
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-09-12 13:14:57 +0200

    improved UDP tests

--- server/test/TestUDP.cpp
+++ server/test/TestUDP.cpp
@@ -11,10 +11,49 @@
 using namespace zouipocar;
 using namespace zouipocar_test;
 
+constexpr int port = 5000;
+
+int send_to_listener(const void *buffer, size_t size) {
+    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sockaddr_in addr_in;
+    addr_in.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr_in.sin_port = htons(port);
+    addr_in.sin_family = AF_INET;
+    sockaddr *addr = reinterpret_cast<sockaddr *>(&addr_in);
+
+    return sendto(s, buffer, size, 0, addr, sizeof(addr_in));
+}
+
+TEST(TestUDP, test_bind_fail) {
+    std::unique_ptr<UDP> first_listener;
+    ASSERT_NO_THROW(first_listener =
+                        std::make_unique<UDP>(port, [](const Fix &fix) {}));
+    ASSERT_THROW(UDP(port, [](const Fix &fix) {}), std::runtime_error);
+}
+
+TEST(TestUDP, test_invalid_callback) {
+    ASSERT_THROW(UDP(port, UDP::UDPCallback()), std::runtime_error);
+}
+
+TEST(TestUDP, test_wrong_packet_size) {
+    bool callback_called = false;
+    UDP udp(port, [&callback_called](const Fix &fix) {
+        callback_called = true;
+    });
+
+    constexpr int size = 3;
+    uint8_t buffer[size];
+    int res = send_to_listener(buffer, size);
+    usleep(1e3);
+
+    ASSERT_NE(res, -1);
+    ASSERT_FALSE(callback_called);
+}
+
 TEST(TestUDP, test_receiver_callback) {
     bool callback_called = false;
     Fix received;
-    UDP udp(5000, [&](const Fix &fix) {
+    UDP udp(port, [&callback_called, &received](const Fix &fix) {
         callback_called = true;
         received = fix;
     });
@@ -25,15 +64,7 @@ TEST(TestUDP, test_receiver_callback) {
     fix.longitude = 2.054971;
     fix.speed = 123;
 
-    int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    sockaddr_in addr_in;
-    addr_in.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr_in.sin_port = htons(5000);
-    addr_in.sin_family = AF_INET;
-    sockaddr *addr = reinterpret_cast<sockaddr *>(&addr_in);
-
-    int res = sendto(s, &fix, sizeof fix, 0, addr, sizeof(addr_in));
-
+    int res = send_to_listener(&fix, sizeof fix);
     usleep(1e3);
 
     ASSERT_NE(res, -1);
